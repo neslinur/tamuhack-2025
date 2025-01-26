e6b00b07da639fcd7ec2992eac3a7a491d1a6de0
Varish Kongara <varish.kongara@gmail.com>
2025-01-25 21:05:07 -0600

    got user location on navbar load

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,58 @@
-import { Link } from 'react-router-dom';
-import "../css/NavBar.css"
-
+import { Link } from "react-router-dom";
+import "../css/NavBar.css";
+import { useState, useEffect } from "react";
 
 function NavBar() {
-    return <nav className="navbar">
-        <div className="navbar-brand">
-            <Link to="/" className="head-nav-link">LifeSaver</Link>
-        </div>
-        <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/event" className="nav-link">Event</Link>
-            <Link to="/offerhelp" className="nav-link">Offer Help</Link>
-        </div>
+  const [userLocation, setUserLocation] = useState(null);
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="head-nav-link">
+          LifeSaver
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/event" className="nav-link">
+          Event
+        </Link>
+        <Link to="/offerhelp" className="nav-link">
+          Offer Help
+        </Link>
+      </div>
+      /* delete later */
+      <div>
+        {userLocation && (
+          <div>
+            <h2>User Location</h2>
+            <p>Latitude: {userLocation.latitude}</p>
+            <p>Longitude: {userLocation.longitude}</p>
+          </div>
+        )}
+      </div>
     </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;

40d12ebb4b62484c9f0c7de39d43edf763d348e5
Varish Kongara <varish.kongara@gmail.com>
2025-01-26 05:13:30 -0600

    Load events in event jsx

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,14 +1,27 @@
 import "../css/Event.css";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Event() {
   const { id } = useParams();
+  const [event, setEvent] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:4090")
+      .then((res) => res.json())
+      .then(
+        (data) => (
+          setEvent(data[0].event_id), console.log(data), console.log(event)
+        )
+      )
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
       <h1>Event Details</h1>
-      <p>You are viewing details for event ID: {id}</p>
+      <p>You are viewing details for event ID: {event}</p>
       {/* You can fetch or display event details based on the ID */}
+      <p>{event}</p>
     </div>
   );
 }

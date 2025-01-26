0112fb3910bdba3efd9506e6d9a5bc9b00ae60e9
IAmAdolfo <aguirre.adolfo.2003@gmail.com>
2025-01-26 01:25:33 -0600

    database setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
-const express = require("express");
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const express = require("express");
+
+const {Sequelize}= require('sequelize');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3002;
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnathorized: false
+    }
+  },
+  logging: false
+})
+
+module.exports = sequelize;
+
+sequelize.sync().then(() => {console.log("Database connected")}).catch((err)=>{console.log(err)});
+
+app.get("/", async (req, res) => {
+  try {
+    const [eventsTable, metadata] = await sequelize.query('SELECT * FROM events');
+    res.json(eventsTable); 
+    console.log("Table obtained");
+
+  } catch (err) {
+    console.log('There was an error:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
-  console.log("Server started on port 3000");
+  console.log(`Server running on port ${port}`);
 });
